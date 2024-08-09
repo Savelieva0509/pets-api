d0b5ae4818b53fcb2e4670b79cdedfee9fd93ea6
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-10 00:52:01 +0300

    Update page.tsx

--- src/app/breeds/page.tsx
+++ src/app/breeds/page.tsx
@@ -11,7 +11,6 @@ export default async function BreedsPage() {
 
   return (
     <main className="max-w-7xl mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-6">Pet Breed Explorer</h1>
       <SearchForm breeds={combinedBreeds} />
     </main>
   );
