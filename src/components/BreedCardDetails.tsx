3a0e8992488d25084bb6ef278970fef576ccc933
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-10 00:50:30 +0300

    Update BreedCardDetails.tsx

--- src/components/BreedCardDetails.tsx
+++ src/components/BreedCardDetails.tsx
@@ -5,7 +5,7 @@ const BreedCardDetails: React.FC<{ breedDetails: BreedDetails }> = ({
   breedDetails,
 }) => {
   return (
-    <div className="max-w-4xl mx-auto p-4 border border-gray-300 rounded-lg shadow-md">
+    <div className="max-w-4xl mx-auto p-4 border border-gray-300 rounded-lg shadow-md bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">{breedDetails.name}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
         {breedDetails.images.map((imageUrl, index) => (
@@ -15,7 +15,7 @@ const BreedCardDetails: React.FC<{ breedDetails: BreedDetails }> = ({
               alt={breedDetails.name}
               layout="fill"
               objectFit="cover"
-              className="rounded-t-lg"
+              className="rounded-lg"
             />
           </div>
         ))}
