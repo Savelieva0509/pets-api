6647978a676f9f7ef3e8abc2be35c1493d81d855
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-09 22:41:42 +0300

    add loader

--- src/app/breeds/page.tsx
+++ src/app/breeds/page.tsx
@@ -1,30 +1,14 @@
 import BreedList from "../../components/BreedList";
-import { fetchDogBreeds, fetchCatBreeds } from "../api/api";
 
-async function getBreeds() {
-  const dogBreeds = await fetchDogBreeds();
-  const catBreeds = await fetchCatBreeds();
 
-  const combinedBreeds = [...dogBreeds, ...catBreeds];
-
-  for (let i = combinedBreeds.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [combinedBreeds[i], combinedBreeds[j]] = [
-      combinedBreeds[j],
-      combinedBreeds[i],
-    ];
-  }
-
-  return combinedBreeds;
-}
 
 export default async function BreedsPage() {
-  const breeds = await getBreeds();
+
 
   return (
-    <main className="max-w-6xl mx-auto p-4">
+    <main className="max-w-7xl mx-auto p-4">
       <h1 className="text-4xl font-bold mb-6">Pet Breed Explorer</h1>
-      <BreedList breeds={breeds} />
+      <BreedList/>
     </main>
   );
 }
