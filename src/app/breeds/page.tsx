76bcb1f81a8b1992ab732dab385fe361009f6020
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-10 16:10:56 +0300

    fix mix breeds

--- src/app/breeds/page.tsx
+++ src/app/breeds/page.tsx
@@ -1,4 +1,3 @@
-// pages/breeds/index.tsx (Серверный компонент)
 import React from "react";
 import { fetchDogBreeds, fetchCatBreeds } from "../../app/api/api";
 import SearchForm from "../../components/SearchForm";
@@ -9,9 +8,19 @@ export default async function BreedsPage() {
   const catBreeds: Breed[] = await fetchCatBreeds();
   const combinedBreeds: Breed[] = [...dogBreeds, ...catBreeds];
 
+  const getMixBreeds = (breeds: Breed[]): Breed[] => {
+    for (let i = breeds.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [breeds[i], breeds[j]] = [breeds[j], breeds[i]];
+    }
+    return breeds;
+  };
+
+  const mixedBreeds = getMixBreeds(combinedBreeds);
+
   return (
     <main className="max-w-7xl mx-auto p-4">
-      <SearchForm breeds={combinedBreeds} />
+      <SearchForm breeds={mixedBreeds} />
     </main>
   );
 }
